193281c7562195513e02880e05dff5a1f137bd05
Marc <mpedascollp@educand.ad>
2025-11-05 09:39:20 +0100

    Inici de la creacio de cursos

---
+++ admin/pages/creacio_cursos.php
@@ -0,0 +1,7 @@
+<section class="card">
+  <h1>Creació de cursos</h1>
+  <p>Aquesta és la pàgina d'<strong>inici</strong>.</p>
+  <ul>
+    <li><a class="btn" href="<?= url('benvingua') ?>">(demo) Enllaç trencat per veure 404</a></li>
+  </ul>
+</section>
